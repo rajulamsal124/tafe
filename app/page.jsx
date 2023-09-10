3958ccd87249500039d592a2d67b501006deb100
Raju Lamsal <rajulamsal62@gmail.com>
2023-09-10 16:13:32 +1000

    home

--- app/page.jsx
+++ app/page.jsx
@@ -15,7 +15,7 @@ import Footer from "@/components/layout/footers/Footer";
 import Preloader from "@/components/common/Preloader";
 export const metadata = {
   title:
-    "Home-2 || Tafegippsland - Professional LMS Online Education Course NextJS Template",
+    "Home || Tafegippsland - Professional LMS Online Education Course NextJS Template",
   description:
     "Elevate your e-learning content with Tafegippsland, the most impressive LMS template for online courses, education and LMS platforms.",
 };
